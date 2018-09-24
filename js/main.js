ea99bd8735f95a4bf032ea668f066631e18b1181
Aliaksandr <31864655+do100Han@users.noreply.github.com>
2018-09-25 00:55:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ var Show = function () {
   document.addEventListener("click", function (e) {
     var target = e.target,
         width = document.documentElement.clientWidth;
-    if (width>=1199 && target == _self.menu || _self.nav.classList.contains("js-active")) {
+    target.preventDefault();
+    if (width<=1199 && target == _self.menu || _self.nav.classList.contains("js-active")) {
       _self.nav.classList.toggle("js-active");
     }
   });
